5121d98603a8f7abdb25470bc70e6ffaf88c0285
kapilnagarkotidev <kapilnagarkoti0@gmail.com>
2026-01-07 01:02:29 +0900

    first c program that print hello world

---
+++ hello.c
@@ -0,0 +1,6 @@
+#include<stdio.h> //Header file that contains input/ output
+
+int main() { // main functions that is must for the program to run
+    printf("Hello, World!");
+    return 0; // for return the print in the main function.
+}
