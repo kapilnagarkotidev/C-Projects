0ce2200b39b2525878b803e4a5a61c170451b5ee
kapilnagarkotidev <kapilnagarkoti0@gmail.com>
2026-01-07 20:32:56 +0900

    variables

---
+++ variablesDatatypes.c/var.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main() {
+    int age = 21; //variables is the name of the container that contains the value.
+    char name[] = "Kapil Nagarkoti"; //jsut like here is name[] is variable name and Kapil Nagarkoti is the value in that container. 
+    // printf("the age is %d", age);
+    printf("%s", name);
+    return 0;
+}
